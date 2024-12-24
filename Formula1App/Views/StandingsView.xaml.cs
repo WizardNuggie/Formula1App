a8efcdc14d8ac6e03e59863822d6ca634e3f40f9
User <User@LAB3-3>
2024-12-24 13:21:46 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Formula1App/Views/StandingsView.xaml.cs
+++ Formula1App/Views/StandingsView.xaml.cs
@@ -1,9 +1,12 @@
+using Formula1App.ViewModels;
+
 namespace Formula1App.Views;
 
 public partial class StandingsView : ContentPage
 {
-	public StandingsView()
+	public StandingsView(StandingsViewModel vm)
 	{
+		this.BindingContext = vm;
 		InitializeComponent();
 	}
 }
