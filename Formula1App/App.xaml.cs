8a6488fa16cc984d21f8ccb41b60e95097487956
User <User@LAB3-3>
2025-01-08 15:16:25 +0200

    aaaaaaa

--- Formula1App/App.xaml.cs
+++ Formula1App/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Formula1App.Models;
 using Formula1App.Services;
+using Formula1App.Styling;
 using Formula1App.ViewModels;
 using Formula1App.Views;
+using Microsoft.Maui.Platform;
 
 namespace Formula1App
 {
